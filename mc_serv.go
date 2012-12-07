a674aa312703821ed5f188f4f7e4849867affeb8
Dustin Sallings <dustin@spy.net>
2012-12-06 16:42:29 -0800

    Functional continuous tap.
    The test here isn't all that classical unity, but I'm not the biggest
    dogma fan.

--- mc_serv.go
+++ mc_serv.go
@@ -22,6 +22,9 @@ type transmissible interface {
 	Transmit(io.Writer) error
 }
 
+// How often to send opaque "heartbeats" on tap streams.
+var tapTickFreq = time.Second
+
 // Given an io.Writer, return a channel that can be fed things that
 // can write themselves to an io.Writer and a channel that will return
 // any encountered error.
@@ -85,6 +88,91 @@ func doStats(w io.Writer, key string) error {
 	return <-errs
 }
 
+func (rh *reqHandler) doTap(req *gomemcached.MCRequest,
+	chpkt chan<- transmissible, cherr <-chan error) error {
+
+	bch := make(chan interface{})
+	mch := make(chan interface{}, 1000)
+
+	rh.currentBucket.Subscribe(bch)
+	defer rh.currentBucket.observer.Unregister(bch)
+
+	ticker := time.NewTicker(tapTickFreq)
+	defer ticker.Stop()
+
+	// defer cleanup vbucket mchs
+	registered := map[uint16]bool{}
+	defer func() {
+		for vbid := range registered {
+			vb := rh.currentBucket.getVBucket(vbid)
+			if vb != nil {
+				vb.observer.Unregister(mch)
+			}
+		}
+	}()
+
+	for {
+		select {
+		case ci := <-bch:
+			// Bucket state change.  Register
+			c := ci.(bucketChange)
+			vb := c.getVBucket()
+			if c.newState == vbActive {
+				vb.observer.Register(mch)
+				registered[vb.vbid] = true
+			} else if vb != nil {
+				vb.observer.Unregister(mch)
+				delete(registered, vb.vbid)
+			}
+		case mi := <-mch:
+			// Send a change
+			m := mi.(mutation)
+			pkt := &gomemcached.MCRequest{
+				Opcode:  gomemcached.TAP_MUTATION,
+				Key:     m.key,
+				VBucket: m.vb,
+			}
+			if m.deleted {
+				pkt.Opcode = gomemcached.TAP_DELETE
+				pkt.Extras = make([]byte, 8) // TODO: fill
+			} else {
+				pkt.Extras = make([]byte, 16) // TODO: fill
+
+				vb := rh.currentBucket.getVBucket(m.vb)
+				if vb != nil {
+					vb.lock.Lock()
+					x := vb.items.Get(&item{key: m.key})
+					vb.lock.Unlock()
+
+					if x == nil {
+						log.Printf("Tapped a missing item, skipping: %s",
+							m.key)
+						continue
+					}
+
+					i := x.(*item)
+					pkt.Body = i.data
+				} else {
+					log.Printf("Change on missing vbucket? %v",
+						m.vb)
+					continue
+				}
+
+			}
+			chpkt <- pkt
+		case <-ticker.C:
+			// Send a noop
+			chpkt <- &gomemcached.MCRequest{
+				Opcode: gomemcached.TAP_OPAQUE,
+				Extras: make([]byte, 8),
+			}
+		case err := <-cherr:
+			return err
+		}
+	}
+	panic("unreachable")
+}
+
 func (rh *reqHandler) HandleMessage(w io.Writer, req *gomemcached.MCRequest) *gomemcached.MCResponse {
 	switch req.Opcode {
 	case gomemcached.QUIT:
@@ -95,6 +183,12 @@ func (rh *reqHandler) HandleMessage(w io.Writer, req *gomemcached.MCRequest) *go
 		return &gomemcached.MCResponse{
 			Body: []byte(VERSION),
 		}
+	case gomemcached.TAP_CONNECT:
+		chpkt, cherr := transmitPackets(w)
+		err := rh.doTap(req, chpkt, cherr)
+		// Currently no way for this to return without failing
+		log.Printf("Error tapping: %v", err)
+		return &gomemcached.MCResponse{Fatal: true}
 	case gomemcached.NOOP:
 		return &gomemcached.MCResponse{}
 	case gomemcached.STAT:
--- ops_test.go
+++ ops_test.go
@@ -301,6 +301,123 @@ func TestQuit(t *testing.T) {
 	}
 }
 
+func TestTapSetup(t *testing.T) {
+	testBucket := newBucket()
+	testBucket.createVBucket(0)
+	testBucket.setVBState(0, vbActive)
+	rh := reqHandler{testBucket}
+
+	req := &gomemcached.MCRequest{
+		Opcode: gomemcached.TAP_CONNECT,
+	}
+
+	// Adjust the tick time for the test since this is really only
+	// the condition that will transmit for this test.
+	origFreq := tapTickFreq
+	tapTickFreq = time.Millisecond
+	defer func() {
+		tapTickFreq = origFreq
+	}()
+
+	res := rh.HandleMessage(&errWriter{io.EOF}, req)
+
+	if !res.Fatal {
+		t.Fatalf("Expected fatality after error tap bringup")
+	}
+}
+
+func TestTapChanges(t *testing.T) {
+	// This test has a couple of loose sync points where it sleeps
+	// waiting for messages to go through since there's no way to
+	// observe the effect of the observation currently.  There
+	// aren't many cases where this would occur, but if this test
+	// starts spuriously failing, that's why, and we'll make it
+	// better.
+
+	testBucket := newBucket()
+	rh := reqHandler{testBucket}
+
+	chpkt := make(chan transmissible, 128)
+	cherr := make(chan error, 1)
+
+	treq := &gomemcached.MCRequest{
+		Opcode: gomemcached.TAP_CONNECT,
+	}
+
+	go rh.doTap(treq, chpkt, cherr)
+
+	vb0 := testBucket.createVBucket(0)
+	testBucket.setVBState(0, vbActive)
+
+	testKey := []byte("testKey")
+
+	mustTransmit := func(m string, typ gomemcached.CommandCode) {
+		select {
+		case m := <-chpkt:
+			req := m.(*gomemcached.MCRequest)
+			if req.Opcode != typ {
+				t.Fatalf("On %v, expected op %v, got %v",
+					m, typ, req.Opcode)
+			}
+		case <-time.After(100 * time.Millisecond):
+			t.Fatalf("No change received at %v.", m)
+		}
+	}
+
+	mustNotTransmit := func(m string) {
+		select {
+		case rv := <-chpkt:
+			t.Fatalf("Unexpected change at %v: %v", m, rv)
+		case <-time.After(50 * time.Millisecond):
+		}
+	}
+
+	sendReq := func(req *gomemcached.MCRequest) {
+		res := rh.HandleMessage(ioutil.Discard, req)
+		if res.Status != gomemcached.SUCCESS {
+			t.Fatalf("Error doing set#1: %v", res)
+		}
+	}
+
+	req := &gomemcached.MCRequest{
+		Opcode: gomemcached.SET,
+		Key:    testKey,
+	}
+
+	// Let tap settle.
+	time.Sleep(100 * time.Millisecond)
+
+	// Verify we get a set on an active vbucket.
+	sendReq(req)
+	mustTransmit("positive set", gomemcached.TAP_MUTATION)
+
+	// Verify we get a delete.
+	req.Opcode = gomemcached.DELETE
+	sendReq(req)
+	mustTransmit("positive delete", gomemcached.TAP_DELETE)
+
+	// Verify a change without a backing item does *not* transmit.
+	vb0.observer.Submit(mutation{key: testKey})
+	mustNotTransmit("negative set")
+
+	// Verify we *don't* get a set on a pending vbucket.
+	testBucket.setVBState(0, vbPending)
+	time.Sleep(100 * time.Millisecond) // Let the state change settle
+	req.Opcode = gomemcached.SET
+	sendReq(req)
+	mustNotTransmit("negative set")
+
+	// Verify a change without a valid vbucket at all doesn't transmit
+
+	// This test is weird because it's a weird thing.  I need to
+	// have an active subscription, and then I forge a message
+	// across it from another vbucket we should ignore.
+	testBucket.setVBState(0, vbActive)
+	time.Sleep(100 * time.Millisecond) // settle
+	vb0.observer.Submit(mutation{vb: 1, key: testKey})
+	mustNotTransmit("no vbucket")
+}
+
 func TestStats(t *testing.T) {
 	testBucket := &bucket{}
 	rh := reqHandler{testBucket}
