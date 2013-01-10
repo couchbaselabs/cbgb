4c76538f7e85a124c6b3dc7abea145a78557dd3c
Steve Yen <steve.yen@gmail.com>
2013-01-09 18:35:04 -0800

    Split out item.go file.
    The vbucket.go file was getting long.

--- vbucket.go
+++ vbucket.go
@@ -2,7 +2,6 @@ package cbgb
 
 import (
 	"bytes"
-	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -46,78 +45,6 @@ func (v VBState) String() string {
 		panic("Invalid vb state")
 	}
 	return vbStateNames[v]
-}
-
-type item struct {
-	key       []byte
-	exp, flag uint32
-	cas       uint64
-	data      []byte
-}
-
-func (i *item) clone() *item {
-	return &item{
-		key:  i.key,
-		exp:  i.exp,
-		flag: i.flag,
-		cas:  i.cas,
-		data: i.data,
-	}
-}
-
-// Serialize everything but the key.
-func (i *item) toValueBytes() []byte {
-	buf := &bytes.Buffer{}
-	if err := binary.Write(buf, binary.BigEndian, i.exp); err != nil {
-		return nil
-	}
-	if err := binary.Write(buf, binary.BigEndian, i.flag); err != nil {
-		return nil
-	}
-	if err := binary.Write(buf, binary.BigEndian, i.cas); err != nil {
-		return nil
-	}
-	if err := binary.Write(buf, binary.BigEndian, uint32(len(i.data))); err != nil {
-		return nil
-	}
-	if _, err := buf.Write(i.data); err != nil {
-		return nil
-	}
-	return buf.Bytes()
-}
-
-func (i *item) fromValueBytes(b []byte) (err error) {
-	buf := bytes.NewBuffer(b)
-	if err = binary.Read(buf, binary.BigEndian, &i.exp); err != nil {
-		return err
-	}
-	if err = binary.Read(buf, binary.BigEndian, &i.flag); err != nil {
-		return err
-	}
-	if err = binary.Read(buf, binary.BigEndian, &i.cas); err != nil {
-		return err
-	}
-	var length uint32
-	if err = binary.Read(buf, binary.BigEndian, &length); err != nil {
-		return err
-	}
-	start := 4 + 4 + 8 + 4
-	i.data = b[start : start+int(length)]
-	return nil
-}
-
-func KeyLess(p, q interface{}) int {
-	return bytes.Compare(p.(*item).key, q.(*item).key)
-}
-
-func CASLess(p, q interface{}) int {
-	if p.(*item).cas < q.(*item).cas {
-		return -1
-	}
-	if p.(*item).cas == q.(*item).cas {
-		return 0
-	}
-	return 1
 }
 
 type VBConfig struct {
