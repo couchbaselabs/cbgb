464cdfc631e26ee6106dff0e532ff7557cac1615
Steve Yen <steve.yen@gmail.com>
2013-01-11 01:07:44 -0800

    Moved transmitPackets() to misc.
    Because it seems very generic / reusable.

--- misc.go
+++ misc.go
@@ -3,10 +3,48 @@ package cbgb
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/url"
 	"os"
 )
+
+type transmissible interface {
+	Transmit(io.Writer) error
+}
+
+// Given an io.Writer, return a channel that can be fed things that
+// can write themselves to an io.Writer and a channel that will return
+// any encountered error.
+//
+// The user of this transmitter *MUST* close the input channel to
+// indicate no more messages will be sent.
+//
+// There will be exactly one error written to the error channel either
+// after successfully transmitting the entire stream (in which case it
+// will be nil) or on any transmission error.
+//
+// Unless your transmissible stream is finite, it's recommended to
+// perform a non-blocking receive of the error stream to check for
+// brokenness so you know to stop transmitting.
+func transmitPackets(w io.Writer) (chan<- transmissible, <-chan error) {
+	ch := make(chan transmissible)
+	errs := make(chan error, 1)
+	go func() {
+		for pkt := range ch {
+			err := pkt.Transmit(w)
+			if err != nil {
+				errs <- err
+				for _ = range ch {
+					// Eat the input
+				}
+				return
+			}
+		}
+		errs <- nil
+	}()
+	return ch, errs
+}
 
 type Bytes []byte
 
@@ -32,6 +70,13 @@ func (a *Bytes) String() string {
 	return string(*a)
 }
 
+func isDir(path string) bool {
+	if finfo, err := os.Stat(path); err != nil || !finfo.IsDir() {
+		return false
+	}
+	return true // TODO: check for writability.
+}
+
 func MutationLogger(ch chan interface{}) {
 	for i := range ch {
 		switch o := i.(type) {
@@ -50,11 +95,4 @@ func MutationLogger(ch chan interface{}) {
 			panic(fmt.Sprintf("Unhandled item to log %T: %v", i, i))
 		}
 	}
-}
-
-func isDir(path string) bool {
-	if finfo, err := os.Stat(path); err != nil || !finfo.IsDir() {
-		return false
-	}
-	return true // TODO: check for writability.
 }
--- server.go
+++ server.go
@@ -18,45 +18,8 @@ type reqHandler struct {
 	currentBucket bucket
 }
 
-type transmissible interface {
-	Transmit(io.Writer) error
-}
-
 // How often to send opaque "heartbeats" on tap streams.
 var tapTickFreq = time.Second
-
-// Given an io.Writer, return a channel that can be fed things that
-// can write themselves to an io.Writer and a channel that will return
-// any encountered error.
-//
-// The user of this transmitter *MUST* close the input channel to
-// indicate no more messages will be sent.
-//
-// There will be exactly one error written to the error channel either
-// after successfully transmitting the entire stream (in which case it
-// will be nil) or on any transmission error.
-//
-// Unless your transmissible stream is finite, it's recommended to
-// perform a non-blocking receive of the error stream to check for
-// brokenness so you know to stop transmitting.
-func transmitPackets(w io.Writer) (chan<- transmissible, <-chan error) {
-	ch := make(chan transmissible)
-	errs := make(chan error, 1)
-	go func() {
-		for pkt := range ch {
-			err := pkt.Transmit(w)
-			if err != nil {
-				errs <- err
-				for _ = range ch {
-					// Eat the input
-				}
-				return
-			}
-		}
-		errs <- nil
-	}()
-	return ch, errs
-}
 
 func (rh *reqHandler) doTap(req *gomemcached.MCRequest,
 	chpkt chan<- transmissible, cherr <-chan error) error {
