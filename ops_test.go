431368513214aa1c38626b35b3d5160744e1c89d
Steve Yen <steve.yen@gmail.com>
2013-02-04 07:34:35 -0800

    BenchmarkParallelSet().

--- ops_test.go
+++ ops_test.go
@@ -713,6 +713,45 @@ func BenchmarkParallelGet(b *testing.B) {
 	wg.Wait()
 }
 
+// Parallel dispatcher invocation timing.
+func BenchmarkParallelSet(b *testing.B) {
+	testBucketDir, _ := ioutil.TempDir("./tmp", "test")
+	defer os.RemoveAll(testBucketDir)
+	testBucket, _ := NewBucket(testBucketDir,
+		&BucketSettings{
+			FlushInterval:   time.Second,
+			SleepInterval:   time.Second,
+			CompactInterval: 10 * time.Second,
+		})
+	defer testBucket.Close()
+	testBucket.CreateVBucket(3)
+
+	rh := reqHandler{testBucket}
+
+	req := &gomemcached.MCRequest{
+		Opcode:  gomemcached.SET,
+		Key:     []byte("k"),
+		VBucket: 3,
+		Body:    []byte("hello"),
+	}
+
+	wg := sync.WaitGroup{}
+	var parallel = 32
+	wg.Add(parallel)
+
+	// Ignore time from above.
+	b.ResetTimer()
+	for worker := 0; worker < parallel; worker++ {
+		go func() {
+			defer wg.Done()
+			for i := 0; i < b.N/parallel; i++ {
+				rh.HandleMessage(nil, req)
+			}
+		}()
+	}
+	wg.Wait()
+}
+
 // Best case dispatcher timing.
 func BenchmarkDispatch(b *testing.B) {
 	testBucketDir, _ := ioutil.TempDir("./tmp", "test")
