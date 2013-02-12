3f07992967bb41147e2292a5610bc99733218d3c
Steve Yen <steve.yen@gmail.com>
2013-02-11 19:27:11 -0800

    Added a fake d3 animated bar graph.

--- static/app.js
+++ static/app.js
@@ -60,8 +60,78 @@ function BucketStatsCtrl($scope, $routeParams, $http) {
       $scope.bucketName = bucketName;
       $scope.bucketStats = data;
       $scope.err = null;
+      go();
     }).
     error(function() {
       $scope.err = restErrorMsg
     });
+}
+
+function go() {
+ var r = 200;
+ var vis = d3.select("#chart");
+
+ var t = 1297110663, // start time (seconds since epoch)
+     v = 70, // start value (subscribers)
+     data = d3.range(33).map(next); // starting dataset
+
+ function next() {
+  return {
+    time: ++t,
+    value: v = ~~Math.max(10, Math.min(90, v + 10 * (Math.random() - .5)))
+  };
+ }
+
+ interval = setInterval(function() {
+    data.shift();
+    data.push(next());
+    redraw();
+ }, 1500);
+
+ var w = 10,
+     h = 80;
+
+ var x = d3.scale.linear()
+    .domain([0, 1])
+    .range([0, w]);
+
+ var y = d3.scale.linear()
+    .domain([0, 100])
+    .rangeRound([0, h]);
+
+ var chart = d3.select("#chart")
+    .attr("class", "chart")
+    .attr("width", w * data.length - 1)
+    .attr("height", h);
+
+ chart.selectAll("rect")
+    .data(data)
+  .enter().append("rect")
+    .attr("x", function(d, i) { return x(i) - .5; })
+    .attr("y", function(d) { return h - y(d.value) - .5; })
+    .attr("width", w)
+    .attr("height", function(d) { return y(d.value); });
+
+ function redraw() {
+  var rect = chart.selectAll("rect")
+      .data(data, function(d) { return d.time; });
+
+  rect.enter().insert("rect", "line")
+      .attr("x", function(d, i) { return x(i + 1) - .5; })
+      .attr("y", function(d) { return h - y(d.value) - .5; })
+      .attr("width", w)
+      .attr("height", function(d) { return y(d.value); })
+    .transition()
+      .duration(1000)
+      .attr("x", function(d, i) { return x(i) - .5; });
+
+  rect.transition()
+      .duration(1000)
+      .attr("x", function(d, i) { return x(i) - .5; });
+
+  rect.exit().transition()
+      .duration(1000)
+      .attr("x", function(d, i) { return x(i - 1) - .5; })
+      .remove();
+ }
 }
