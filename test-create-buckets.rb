e07fd29a8cdac06f40bb160a73b983a2754026fd
Steve Yen <steve.yen@gmail.com>
2013-02-20 19:27:55 -0800

    Helper test script to create N buckets.

---
+++ test-create-buckets.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+h = ARGV[1] || "http://127.0.0.1:8077"
+n = (ARGV[0] || "200").to_i
+n.times.each do |i|
+  bucketName = "b" + i.to_s.rjust(4, '0')
+  p "bucketName #{bucketName}"
+  p `curl -X POST #{h}/api/buckets -F bucketName=#{bucketName}`
+end
+
