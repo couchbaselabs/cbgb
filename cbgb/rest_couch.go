87770a38148f5839cab494c9b401e71d8fbd1036
Steve Yen <steve.yen@gmail.com>
2013-02-22 16:25:51 -0800

    REST api for /{db}/{docid} GET.

--- cbgb/rest_couch.go
+++ cbgb/rest_couch.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/couchbaselabs/cbgb"
+	"github.com/dustin/gomemcached"
 	"github.com/gorilla/mux"
 )
 
@@ -50,43 +53,96 @@ func couchAllDbs(w http.ResponseWriter, r *http.Request) {
 }
 
 func couchDbAllDocs(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbBulkDocs(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbBulkGet(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbChanges(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbRevsDiff(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbGetLocalDoc(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbPutLocalDoc(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbDelLocalDoc(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbGetDesignDoc(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbPutDesignDoc(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbDelDesignDoc(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbGetDoc(w http.ResponseWriter, r *http.Request) {
+	_, _, bucket, docId := checkDocId(w, r)
+	if bucket == nil || len(docId) <= 0 {
+		return
+	}
+	res := cbgb.GetItem(bucket, []byte(docId), cbgb.VBActive)
+	if res == nil || res.Status != gomemcached.SUCCESS {
+		http.Error(w, "Not Found", 404)
+		return
+	}
+	w.Write(res.Body)
 }
 
 func couchDbPutDoc(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
 }
 
 func couchDbDelDoc(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "unimplemented", 501)
+}
+
+func checkDb(w http.ResponseWriter, r *http.Request) (
+	vars map[string]string, bucketName string, bucket cbgb.Bucket) {
+	vars = mux.Vars(r)
+	bucketName, ok := vars["db"]
+	if !ok {
+		http.Error(w, "missing db parameter", 400)
+		return vars, "", nil
+	}
+	bucket = buckets.Get(bucketName)
+	if bucket == nil {
+		http.Error(w, fmt.Sprintf("no db: %v", bucketName), 404)
+		return vars, bucketName, nil
+	}
+	return vars, bucketName, bucket
+}
+
+func checkDocId(w http.ResponseWriter, r *http.Request) (
+	vars map[string]string, bucketName string, bucket cbgb.Bucket, docId string) {
+	vars, bucketName, bucket = checkDb(w, r)
+	if bucket == nil {
+		return vars, bucketName, bucket, ""
+	}
+	docId, ok := vars["docid"]
+	if !ok {
+		http.Error(w, "missing docid parameter", 400)
+		return vars, bucketName, bucket, ""
+	}
+	return vars, bucketName, bucket, docId
 }
