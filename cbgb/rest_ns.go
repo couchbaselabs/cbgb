165350174b447884a8f2da3e96cdb2ab5fed53fc
Steve Yen <steve.yen@gmail.com>
2013-03-05 09:04:57 -0800

    Register mux handlers for all the ns templates.

--- cbgb/rest_ns.go
+++ cbgb/rest_ns.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"path/filepath"
+	"strings"
 	"text/template"
 
 	"github.com/gorilla/mux"
@@ -13,14 +14,25 @@ var nsTemplates *template.Template
 
 func restNSAPI(r *mux.Router) {
 	dir := filepath.Join(*staticPath, "ns", "pools_default*.json")
-	nsTemplates = template.Must(template.ParseGlob(dir))
+	files, err := filepath.Glob(dir)
+	if err != nil {
+		panic(fmt.Sprintf("glob ns dir: %v, err: %v", dir, err))
+	}
+	for _, file := range files {
+		if strings.HasPrefix(file, *staticPath) {
+			p := file[len(*staticPath)+len("/ns"):]
+			p = strings.Replace(p, "_", "/", -1)
+			p = strings.Replace(p, ".json", "", -1)
+			r.HandleFunc(p, restNSHandler).Methods("GET")
+		}
+	}
 
-	r.HandleFunc("/pools/default",
-		nsGetPoolsDefault).Methods("GET")
+	nsTemplates = template.Must(template.ParseGlob(dir))
 }
 
-func nsGetPoolsDefault(w http.ResponseWriter, r *http.Request) {
-	if err := nsTemplates.ExecuteTemplate(w, "pools_default.json", nil); err != nil {
+func restNSHandler(w http.ResponseWriter, r *http.Request) {
+	templateName := strings.Replace(r.URL.Path[1:], "/", "_", -1) + ".json"
+	if err := nsTemplates.ExecuteTemplate(w, templateName, nil); err != nil {
 		http.Error(w, fmt.Sprintf("ExecuteTemplate err: %v", err), 500)
 		return
 	}
