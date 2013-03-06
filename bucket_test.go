b1d8e2597143d4d32e4d06ae3a935cb8a9f03502
Steve Yen <steve.yen@gmail.com>
2013-03-06 15:33:34 -0800

    TestMemoryOnlyBucket().

--- bucket_test.go
+++ bucket_test.go
@@ -547,3 +547,68 @@ func TestBucketSettingsSafeView(t *testing.T) {
 		t.Errorf("safe view should not expose password salt, got: %v", sv)
 	}
 }
+
+func TestMemoryOnlyBucket(t *testing.T) {
+	testBucketDir, _ := ioutil.TempDir("./tmp", "test")
+	defer os.RemoveAll(testBucketDir)
+
+	b0, err := NewBucket(testBucketDir,
+		&BucketSettings{
+			NumPartitions: MAX_VBUCKETS,
+			MemoryOnly:    true,
+		})
+	if err != nil {
+		t.Errorf("expected NewBucket to work, got: %v", err)
+	}
+
+	r0 := &reqHandler{currentBucket: b0}
+	vb0, _ := b0.CreateVBucket(2)
+	b0.SetVBState(2, VBActive)
+
+	if 0 != vb0.stats.Items {
+		t.Errorf("expected to have 0 items initially")
+	}
+
+	testLoadInts(t, r0, 2, 5)
+	testExpectInts(t, r0, 2, []int{0, 1, 2, 3, 4}, "initial data load")
+
+	if 5 != vb0.stats.Items {
+		t.Errorf("expected to have 5 items")
+	}
+
+	err = b0.Flush()
+	if err != nil {
+		t.Errorf("expected Flush to work, got: %v", err)
+	}
+
+	if 5 != vb0.stats.Items {
+		t.Errorf("expected to have 5 items still after flushing")
+	}
+
+	b0.Close()
+
+	b1, err := NewBucket(testBucketDir,
+		&BucketSettings{
+			NumPartitions: MAX_VBUCKETS,
+			MemoryOnly:    true,
+		})
+	if err != nil {
+		t.Errorf("expected NewBucket to work, got: %v", err)
+	}
+	defer b1.Close()
+
+	b1.Load()
+	vb1 := b1.GetVBucket(2)
+	vbs1 := vb1.GetVBState()
+	if vb1 == nil {
+		t.Errorf("expected vbucket 2 to be there after re-load")
+	}
+	if vbs1 != VBActive {
+		t.Errorf("expected vbucket 2 to be active, got: %v", vbs1)
+	}
+	if 0 != vb1.stats.Items {
+		t.Errorf("expected to have 0 items after loading, got: %v", vb1.stats.Items)
+	}
+	r1 := &reqHandler{currentBucket: b1}
+	testExpectInts(t, r1, 2, []int{}, "data re-load")
+}
