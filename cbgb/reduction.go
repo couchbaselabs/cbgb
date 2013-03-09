dfd408185d6bcd467c4e77344924ac5257559c5c
Dustin Sallings <dustin@spy.net>
2013-03-09 13:27:09 -0800

    Implementation of _stats
    cbugg: close bug-448

--- cbgb/reduction.go
+++ cbgb/reduction.go
@@ -12,6 +12,7 @@ func newReducer() *otto.Otto {
 	must(o.Set("sum", javascriptSum))
 	must(o.Set("_sum", javascriptReduceSum))
 	must(o.Set("_count", javascriptReduceCount))
+	must(o.Set("_stats", javascriptReduceStats))
 	return o
 }
 
@@ -96,6 +97,97 @@ func javascriptReduceCount(call otto.FunctionCall) otto.Value {
 		rv += zeroate(i)
 	}
 	return ottoMust(otto.ToValue(rv))
+}
+
+type statsResult struct {
+	sum, min, max, sumsqr float64
+	count                 int
+}
+
+func (s statsResult) toMap() map[string]float64 {
+	return map[string]float64{
+		"sum":    s.sum,
+		"count":  float64(s.count),
+		"min":    s.min,
+		"max":    s.max,
+		"sumsqr": s.sumsqr,
+	}
+}
+
+func (s statsResult) toOtto() otto.Value {
+	o := otto.New()
+	return ottoMust(o.ToValue(s.toMap()))
+}
+
+func (s *statsResult) load(ob interface{}) {
+	m, ok := ob.(map[string]interface{})
+	if !ok {
+		return
+	}
+	s.sum = zeroate(m["sum"])
+	s.count = int(zeroate(m["count"]))
+	s.min = zeroate(m["min"])
+	s.max = zeroate(m["max"])
+	s.sumsqr = zeroate(m["sumsqr"])
+}
+
+func (s *statsResult) Add(from statsResult) {
+	s.sum += from.sum
+	s.count += from.count
+	s.min = math.Min(s.min, from.min)
+	s.max = math.Max(s.max, from.max)
+	s.sumsqr += from.sumsqr
+}
+
+func javascriptReduceStats(call otto.FunctionCall) otto.Value {
+	rere, err := call.Argument(2).ToBoolean()
+	if err != nil {
+		return ottoMust(otto.ToValue(fmt.Sprintf("Error getting rere flag: %v", err)))
+	}
+
+	ob, err := call.Argument(1).Export()
+	if err != nil {
+		return ottoMust(otto.ToValue(fmt.Sprintf("Error getting stuff: %v", err)))
+	}
+	l, ok := ob.([]interface{})
+	if !ok {
+		return ottoMust(otto.ToValue(fmt.Sprintf("unhandled %v/%T", ob, ob)))
+	}
+
+	rv := statsResult{}
+
+	if len(l) == 0 {
+		return rv.toOtto()
+	}
+
+	if rere {
+		// Rereduction goes here.
+		rv.load(l[0])
+		for i := 1; i < len(l); i++ {
+			ob := statsResult{}
+			ob.load(l[i])
+			rv.Add(ob)
+		}
+		return rv.toOtto()
+	}
+
+	// Initial reduction
+	rv.count = 1
+	rv.sum = zeroate(l[0])
+	rv.min = rv.sum
+	rv.max = rv.sum
+	rv.sumsqr = rv.sum * rv.sum
+
+	for i := 1; i < len(l); i++ {
+		v := zeroate(l[i])
+		rv.count++
+		rv.sum += v
+		rv.min = math.Min(rv.min, v)
+		rv.max = math.Max(rv.max, v)
+		rv.sumsqr += (v * v)
+	}
+
+	return rv.toOtto()
 }
 
 func must(err error) {
--- cbgb/reduction_tests.js
+++ cbgb/reduction_tests.js
@@ -1,10 +1,54 @@
+// Helper function I wrote and can probably kill now.
+
 assert("basic sum", sum(1, 2, 3), 6);
 assert("empty sum", sum(), 0);
 assert("NaN sum", sum(1, NaN, 1), 2);
 assert("array sum", sum([1, 2, 3]), 6);
 
+//
+// _sum
+//
+
 assert("builtin_sum", _sum(["a", "b"], [1, 2, 3], false), 6);
 assert("builtin_sum rere", _sum(["a", "b"], [1, 2, 3], true), 6);
 
-assert("builtin_reduce", _count(["a", "b"], [1, 2, 3], false), 3);
-assert("builtin_reduce rere", _count(["a", "b"], [1, 2, 3], true), 6);
+//
+// _count
+//
+
+assert("builtin_reduce _count", _count(["a", "b"], [1, 2, 3], false), 3);
+assert("builtin_reduce _count rere", _count(["a", "b"], [1, 2, 3], true), 6);
+
+//
+// _stats
+//
+
+// First, if we pass it nothing, does it freak out?
+assert("builtin_stats empty", _stats([], [], false).count, 0);
+
+// Try a single, three item pass and verify all results.
+var statsed = _stats(["a", "b", "c"],
+                     [2, 5, 6],
+                     false);
+
+assert("builtin_stats count", statsed.count, 3);
+assert("builtin_stats sum", statsed.sum, 13);
+assert("builtin_stats sumsqr", statsed.sumsqr, 65);
+assert("builtin_stats min", statsed.min, 2);
+assert("builtin_stats max", statsed.max, 6);
+
+// Now we get fancy, two passes, different sizes of items, verify results.
+var statsets = [[1, 2], [2, 9], [19, 19, 187]];
+
+var firstpasses = [];
+for (var i = 0; i < statsets.length; i++) {
+    firstpasses.push(_stats(["a", "b"], statsets[i], false));
+}
+
+statsed = _stats(["a", "b"], firstpasses, true);
+
+assert("builtin_stats rere count", statsed.count, 7);
+assert("builtin_stats rere sum", statsed.sum, 239);
+assert("builtin_stats rere sumsqr", statsed.sumsqr, 35781);
+assert("builtin_stats rere min", statsed.min, 1);
+assert("builtin_stats rere max", statsed.max, 187);
