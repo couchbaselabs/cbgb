a35e68b867d5e5b0a366532cd43c7086689b7988
Dustin Sallings <dustin@spy.net>
2013-11-16 22:36:41 -0800

    Proper build tag fix

--- signals_bsd.go
+++ signals_bsd.go
@@ -10,7 +10,7 @@
 // implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-// +build !linux !windows
+// +build !linux,!windows
 
 package main
 
